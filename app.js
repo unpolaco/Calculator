705ba9a6bf498f546277b8d198d844a474ca2b19
Michal Borowski <unpolacoo@gmail.com>
2019-12-09 23:23:45 +0100

    Defined operators

--- app.js
+++ app.js
@@ -2,18 +2,29 @@
 const score = [];
 
 const numbers = [...document.querySelectorAll(".number")];
-
-
+const operators = [...document.querySelectorAll(".operator")];
+const scoreDisplay = document.querySelector(".score")
 
 
 numbers.forEach( number => {
-  number.addEventListener("click", function() {
+  number.addEventListener("click", ()=> {
     
-    score.push(this.textContent);
+    score.push(Number(number.textContent));
+    console.log(score);
+    scoreDisplay.value = score;
+    console.log(scoreDisplay);
+  })
+})
+
+
+operators.forEach( operator => {
+  operator.addEventListener("click", function() {
+      
+    score.push(operator.textContent);
     console.log(score);
   })
 })
   
-
-  
-
+document.querySelector('#test').addEventListener('click', ()=>{
+  console.log(scoreDisplay.value)
+})
