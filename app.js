5ae13d6c0e4df51a85042525cdd85046f3567790
Michal Borowski <unpolacoo@gmail.com>
2019-12-09 23:37:31 +0100

    Changing entered digits to string. When calling the operator, add numbers to array

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const score = [];
+let insertedNumber = "";
 
 const numbers = [...document.querySelectorAll(".number")];
 const operators = [...document.querySelectorAll(".operator")];
@@ -9,18 +10,25 @@ const scoreDisplay = document.querySelector(".score")
 numbers.forEach( number => {
   number.addEventListener("click", ()=> {
     
-    score.push(Number(number.textContent));
-    console.log(score);
-    scoreDisplay.value = score;
-    console.log(scoreDisplay);
+    insertedNumber += number.textContent;
+    console.log(typeof(insertedNumber));
+    
+    // score.push(Number(number.textContent));
+    // console.log(score);
+    // scoreDisplay.value = score;
+    // console.log(scoreDisplay);
   })
 })
 
 
 operators.forEach( operator => {
   operator.addEventListener("click", function() {
-      
+
+    score.push(Number(insertedNumber));
+    insertedNumber = ""
     score.push(operator.textContent);
+    
+    
     console.log(score);
   })
 })
