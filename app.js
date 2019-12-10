eccd9af54ac1d5809e4bf73843293ef9909204df
Michal Borowski <unpolacoo@gmail.com>
2019-12-10 21:40:36 +0100

    funcionality - operations on numbers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
-const score = [];
+
+const partialScore = [];  
 let insertedNumber = "";
+let insertedOperator = "";
 
 const numbers = [...document.querySelectorAll(".number")];
 const operators = [...document.querySelectorAll(".operator")];
@@ -11,12 +13,11 @@ numbers.forEach( number => {
   number.addEventListener("click", ()=> {
     
     insertedNumber += number.textContent;
-    console.log(typeof(insertedNumber));
+    // console.log(typeof(insertedN umber));
+    scoreDisplay.value = insertedNumber;
+    console.log(insertedNumber);
     
-    // score.push(Number(number.textContent));
-    // console.log(score);
-    // scoreDisplay.value = score;
-    // console.log(scoreDisplay);
+
   })
 })
 
@@ -24,15 +25,38 @@ numbers.forEach( number => {
 operators.forEach( operator => {
   operator.addEventListener("click", function() {
 
-    score.push(Number(insertedNumber));
-    insertedNumber = ""
-    score.push(operator.textContent);
+
+    if(operator.textContent == "=") {
+      partialScore.push(Number(insertedNumber));
+      insertedNumber = "";
+      // console.log(score.join(" "));
+      
+      const finalScore = eval(partialScore.join(""));
+      scoreDisplay.value = finalScore;
+      // for(let i=0; i<score.length; i++) {
+      //   if(score[i]%2!=0) {
+          
+      //   }
+      // }
+      partialScore.length = 0;
+      console.log(partialScore);
+      console.log(finalScore);
+      
+
+
+    } else {
+      partialScore.push(Number(insertedNumber));
+      insertedNumber = "";
+      partialScore.push(operator.textContent);
+    }
     
-    
-    console.log(score);
+    console.log(partialScore);
   })
 })
   
 document.querySelector('#test').addEventListener('click', ()=>{
   console.log(scoreDisplay.value)
 })
+
+
+
